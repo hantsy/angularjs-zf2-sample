85908b5c68d5f06f952c893b4e63ecfe9e0eb3d9
Alefe Variani <alefevariani18@gmail.com>
2015-09-23 15:14:54 -0300

    Implementation PSRs and syntax php array

--- module/Album/src/Album/Model/Person.php
+++ module/Album/src/Album/Model/Person.php
@@ -1,11 +1,4 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Album\Model;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -16,8 +9,8 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\DiscriminatorColumn(name="person_type", type="string")
  * @ORM\DiscriminatorMap({"A"="Artist", "P"="Person"})
  */
-class Person {
-
+class Person
+{
     /**
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -25,23 +18,28 @@ class Person {
      */
     private $id;
 
-    /** @ORM\Column(type="string") */
+    /** 
+     * @ORM\Column(type="string")
+     */
     private $name;
 
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
 
-    public function setId($id) {
+    public function setId($id)
+    {
         $this->id = $id;
     }
 
-    public function setName($name) {
+    public function setName($name)
+    {
         $this->name = $name;
     }
-
 }
--- module/Album/src/Album/Model/Song.php
+++ module/Album/src/Album/Model/Song.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Album\Model;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -7,8 +6,8 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  */
-class Song {
-
+class Song
+{
     /**
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
@@ -16,48 +15,59 @@ class Song {
      */
     private $id;
 
-    /** @ORM\Column(type="string") */
+    /** 
+     * @ORM\Column(type="string") 
+     */
     private $name;
-    
-     /** @ORM\Column(type="string") */
+
+    /** 
+     * @ORM\Column(type="string") 
+     */
     private $duration;
 
     /**
      * @ORM\ManyToOne(targetEntity="Album", inversedBy="songs")
-     * @ORM\JoinColumn(name="album_id") */
+     * @ORM\JoinColumn(name="album_id") 
+     */
     private $album;
 
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
 
-    public function setId($id) {
+    public function setId($id)
+    {
         $this->id = $id;
     }
 
-    public function setName($name) {
+    public function setName($name)
+    {
         $this->name = $name;
     }
-    
-    public function getAlbum() {
+
+    public function getAlbum()
+    {
         return $this->album;
     }
 
-    public function setAlbum($album) {
+    public function setAlbum($album)
+    {
         $this->album = $album;
     }
 
-    public function getDuration() {
+    public function getDuration()
+    {
         return $this->duration;
     }
 
-    public function setDuration($duration) {
+    public function setDuration($duration)
+    {
         $this->duration = $duration;
     }
-
-
 }
